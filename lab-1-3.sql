2372b5ade92d5bdae2594affdbee93e157aaf8ac
Jodi Kraushar <jodi.kraushar@kellogg.northwestern.edu>
2024-01-10 15:44:37 +0000

    completed lab 1 team sql queries

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -1,5 +1,10 @@
 -- Which three teams had the losingest seasons?
 
+SELECT year, name, losses 
+FROM teams 
+ORDER BY losses DESC 
+LIMIT 3;
+
 -- Expected result:
 --
 -- +------+----------------------+------+--------+
